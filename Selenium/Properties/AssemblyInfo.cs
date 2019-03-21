5609e89373167d1fae117d3c2e3715b7bf68e9b7
JacobLRL <JLittle@spartaglobal.com>
2019-03-21 10:52:56 +0000

    initial commit

---
+++ Selenium/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Selenium")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Selenium")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("398ada04-7467-4c3b-9047-4d8efdeae779")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Selenium/SeleniumBBCLoginTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Selenium
+{
+    /// <summary>
+    /// Summary description for SeleniumBBCLoginTest
+    /// </summary>
+    [TestFixture]
+    public class SeleniumBBCLoginTest
+    {
+        [Test]
+        public void SeleniumWalkThrough() {
+            using (IWebDriver driver = new ChromeDriver()) {
+                driver.Manage().Window.Maximize();
+                driver.Navigate().GoToUrl("https://bbc.co.uk");
+                IWebElement loginButton = driver.FindElement(By.Id("idcta-link"));
+                loginButton.Click();
+                IWebElement usernameField = driver.FindElement(By.Id("user-identifier-input"));
+                usernameField.SendKeys("Testing@JakeLRL.com");
+                IWebElement passwordField = driver.FindElement(By.Id("password-input"));
+                passwordField.SendKeys("12345678");
+                IWebElement submitButton = driver.FindElement(By.Id("submit-button"));
+                submitButton.Click();
+                IWebElement passwordError = driver.FindElement(By.Id("form-message-password"));
+                Assert.AreEqual("Sorry, that password isn't valid. Please include a letter.", passwordError.Text);
+            }
+        }
+        
+    }
+}
